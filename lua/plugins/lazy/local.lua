a3404303cec069a261e86a81c7789cd930cefc46
tr1x_em <kmtechnical2006@gmail.com>
2024-11-23 15:56:48 +0530

    Update 22 Nov 2025

--- lua/plugins/lazy/local.lua
+++ lua/plugins/lazy/local.lua
@@ -1,14 +1,16 @@
 local local_plugins = {
     {
-        "theprimeagen/harpoon",  -- Update to the online repository
-        lazy = true,
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = false,
         config = function()
             local harpoon = require("harpoon")
 
             harpoon:setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+            vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
             vim.keymap.set("n", "<C-M-h>", function() harpoon:list():select(1) end)
             vim.keymap.set("n", "<C-M-j>", function() harpoon:list():select(2) end)
@@ -23,4 +25,3 @@ local local_plugins = {
 }
 
 return local_plugins
-
