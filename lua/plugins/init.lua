5c690a3d8792b88c0b504af080449245f9ceddf5
tr1x_em <admin@trix.is-a.dev>
2024-11-30 10:38:20 +0530

    Greatest Commit Ever

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,53 +2,60 @@ require("plugins.set")
 require("plugins.remap")
 require("plugins.lazy_init")
 require('lualine').setup()
-require("startup").setup({theme = "evil"}) -- put theme name here
+require("startup").setup({ theme = "evil" }) -- put theme name here
 
 local cmp = require('cmp')
 local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
-cmp.setup({
-  mapping = {
-    ["<Tab>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
-      end
-    end),
-  },
-})
 
 -- lspkind.lua
 local lspkind = require("lspkind")
 -- cmp.lua
 cmp.setup {
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = "symbol",
-      max_width = 50,
-      symbol_map = { Copilot = "" }
-    })
-  }
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = "symbol",
+            max_width = 50,
+            symbol_map = { Copilot = "" }
+        })
+    }
 }
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-end
 cmp.setup({
-  mapping = {
-    ["<Tab>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      else
-        fallback()
-      end
-    end),
-  },
+    mapping = {
+        ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),   -- Next item
+        ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),   -- Previous item
+
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            if cmp.visible() and has_words_before() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(), -- Close menu in insert mode
+            c = cmp.mapping.close(), -- Close menu in command-line mode
+        }),
+
+        -- Open the completion menu manually
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+
+        -- Scroll through documentation (if available)
+        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }), -- Scroll up
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),  -- Scroll down
+
+        -- Fallback for `<S-Tab>` for navigating backward
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
 })
 
 
@@ -94,7 +101,7 @@ autocmd('TextYankPost', {
 local lspconfig = require('lspconfig')
 local on_attach = function()
 end
-autocmd({"BufWritePre"}, {
+autocmd({ "BufWritePre" }, {
     group = trixgroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
@@ -127,4 +134,4 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
